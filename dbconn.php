3f2f516c6b2d6594e6e9d9edd322fa1acab8292f
Suman-9609 <74442293+Suman-9609@users.noreply.github.com>
2020-11-16 15:06:08 +0530

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,18 @@
 <?php
+//Develop Database connection
+// $conn = mysqli_connect("localhost","root","","iscool_db");
 
-$conn = mysqli_connect("localhost","root","","iscool_db");
+// if(!$conn) {
+//     die("connection failed.");
+// }
+
+
+// Remote databas connection
+$server = "remotemysql.com";
+$user = "P4jnEZCmC2";
+$pass = "k06odNjtc7";
+$db = "P4jnEZCmC2";
+$conn = mysqli_connect($server, $user, $pass, $db);
 
 if(!$conn) {
     die("connection failed.");
